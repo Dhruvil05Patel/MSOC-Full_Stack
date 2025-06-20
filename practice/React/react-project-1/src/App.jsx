3cde75ef79e6365a80c063f085ae34e0b3e46058
Dhruvil05Patel <dhruvil1405patel@gmail.com>
2025-06-20 22:47:24 +0530

    React_Day5

--- practice/React/react-project-1/src/App.jsx
+++ practice/React/react-project-1/src/App.jsx
@@ -4,10 +4,26 @@ import './App.css'
 import dhruvilpic from './assets/dhruvil.jpg'
 import nishthapic from './assets/nishtha.jpeg'
 import TimeCounter from './components/TimeCounter'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element : <UserCard/>,
+  },
+  {
+    path:"/count",
+    element : <TimeCounter/>,
+  },
+])
 
 const App = () => {
+
   return (
     <div className='container'>
+      <RouterProvider router={router} />
       <div className="usercard">
           <UserCard name='Dhruvil' desc='I am Dhruvil Patel' pic={dhruvilpic}/>
           <UserCard name='Nishtha' desc='I am Nishtha Icecreamwala' pic={nishthapic}/>
--- practice/React/react-project-1/src/components/UserCard.jsx
+++ practice/React/react-project-1/src/components/UserCard.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 import './UserCard.css'
 
 const UserCard = (props) => {
+
+  function handlePic() {
+    alert("dont try to spy on mee")
+  }
+
   return (
     <div className='user-container'>
       <p id='userTitle'>{props.name}</p>
-      <img id='userPic' src={props.pic} alt="dhruvil" />
+      <img onMouseOver={handlePic} id='userPic' src={props.pic} alt="dhruvil" />
       <p id='user-desc'>{props.desc}</p>
     </div>
   )
