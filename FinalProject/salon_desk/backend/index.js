b4f0d32d2fae0a838cff3adee80907ccf923fe0e
Dhruvil05Patel <dhruvil1405patel@gmail.com>
2025-08-02 22:31:35 +0530

    FINAL

--- FinalProject/salon_desk/backend/index.js
+++ FinalProject/salon_desk/backend/index.js
@@ -27,9 +27,6 @@ app.use(cors({
   optionsSuccessStatus: 204
 }))
 
-// Handle preflight requests explicitly for Safari
-app.options('*', cors())
-
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ extended: true, limit: '10mb' }))
 
